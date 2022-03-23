0606f3bb4621638c4df0232219b031bb1bd66959
Hericson Araújo <hericsonaraujo2013@gmail.com>
2022-03-23 22:21:42 +0000

    Adicionar a questão 1

--- Conceitos Básicos/problema 1.py
+++ Conceitos Básicos/problema 1.py
@@ -8,21 +8,4 @@
 
 # O circuito de Interlagos precisa de 82 voltas para completar 353.34 Km
 
-import math
-
-# Armazenar o nome do circuito
-nome_circuito = input("Qual o nome do circuito? ")
-# Armazenar a extensão do circuito
-extensao_circuito = float(input("Qual a extensão do circuito? "))
-# Armazenar o percurso mínimo
-percurso_minimo = 350
-
-numero_voltas = math.ceil(percurso_minimo / extensao_circuito)
-
-extensao_corrida = numero_voltas * extensao_circuito
-
-print("O circuito de", nome_circuito,
-     "precisa de", numero_voltas,
-     "voltas para completar",
-     round(extensao_corrida, 2), "Km")
 
